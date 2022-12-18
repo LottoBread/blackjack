6628a940429a1a13d4c56c1d28263a28a234c4dc
LottoRed <lotto@lotto-red.com>
2022-12-18 15:02:20 -0800

    idklol

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <ctime>
+
+using namespace std;
+//string sign[] = { "Diamonds", "Hearts", "Clubs", "Spades" };
+string value[] = { "Ace", "Two", "Three","Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+int cardDeal()
+{
+int cardValue = 1+(rand() % 13);
+srand(time(0));
+return cardValue;
+}
+
+string randomcard()
+{
+    string card;
+    card = value[cardDeal()];
+    return card;
+
+}
+
+int clip()
+{
+    int lowestCard = 1;
+    int highestCard = 10;
+  return std::max(lowestCard, std::min(cardDeal()+1, highestCard));
+}
+
+int main()
+{
+    cardDeal();
+    int numberofcards = 52;
+    int trueValue = clip();
+
+    cout << "You drew a " << randomcard() << endl << trueValue << endl;
+}
